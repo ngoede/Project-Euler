9f24a155c511255448fd77f24103631fefb5caef
Nick Goede <ngoede@gmail.com>
2011-04-03 22:15:30 -0400

    Created new project for problem 24

---
+++ Project-Euler24/Project-Euler24/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project_Euler24 {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
