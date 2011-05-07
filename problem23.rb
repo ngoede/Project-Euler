af7fb03ff8e5509596606024e42e4ec7ee229f7b
Nick Goede <ngoede@gmail.com>
2011-05-07 00:17:55 -0400

    Divisors is progress

--- problem23.rb
+++ problem23.rb
@@ -1,22 +1,40 @@
-primes = [2]
-(3..28123).each {|x| primes << x if !primes.any? {|i| x % i == 0}}
-primes = [1] + primes
-p primes
-puts "Computed primes..."
-
-def divisors(n, primes)
-  less_primes = primes.take_while {|x| x < n}
-  prime_divisors = less_primes.find_all {|i| (n % i) == 0 }
-  prime_divisors.map {|x| n / x } + prime_divisors
+def primes(up_to)
+  primes = []
+  canidates = (2..up_to).to_a
+  p = 0
+  while p < Math.sqrt(up_to)
+    p = canidates.shift
+    primes << p
+    canidates = canidates.select {|x| x % p != 0 }
+  end
+  
+  primes + canidates
 end
 
-def abundant?(n, primes)
-  divs = divisors(n, primes)
-  sum = divs.reduce(:+) - n
-  sum > n
+$prime_nums = primes 28123
+
+def divisors(n)
+  less_primes = $prime_nums.take_while {|p| p <= Math.sqrt(n)}
+  divisors = [1]
+  less_primes.each do |x|
+    if n % x == 0 then
+      divisors << x
+      i = n / x
+      while (n % i) == 0 and i >= x
+        divisors << i
+        divisors << n / i
+        i = i / x
+      end
+    end
+  end
+  divisors | less_primes.combination(2).map {|i| i[0] * i[1]}.find_all {|x| n % x == 0}
 end
 
-abundant = (2..28123).find_all {|i| abundant?(i, primes)}
+def abundant?(n)
+  divisors(n).reduce(:+) > n
+end
+
+abundant = (2..28123).find_all {|i| abundant?(i)}
 puts "Found all abundant in range..."
 sum_of_abundant = []
 abundant.each do |x|
@@ -29,4 +47,4 @@ puts "Sum of two abundant numbers found..."
 
 puts abundant.length
 puts sum_of_abundant.length
-puts ((1..28123).to_a - sum_of_abundant).inject {|i, sum| i + sum}
+puts ((1..28123).to_a - sum_of_abundant).reduce(:+)
