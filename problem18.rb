36d7f0cade054c9741af06c1d8d9ffb68ad998a0
Nick Goede <ngoede@gmail.com>
2011-05-11 21:24:35 -0400

    problem 18 done

--- problem18.rb
+++ problem18.rb
@@ -24,19 +24,34 @@ short_text = <<EOF
 EOF
 
 class Node
-  attr :value
+  attr_reader :value
+  attr_reader :left
+  attr_reader :right
   
-  def initialize(value, left, right)
+  NumberToLookAhead = 3
+  
+  def initialize	(value, y, x, raw)
     @value = value
-    @left = left
-    @right = right
+    if raw.length - 1 > y then
+      @left = Node.new(raw[y+1][x], y + 1, x, raw)
+      @right = Node.new(raw[y+1][x+1], y + 1, x + 1, raw)
+    end
   end
   
-  def to_s
-    "#{@value} \n#{@left.to_s} #{@right.to_s}"
+  def max
+    score_right = right.nil? ? 0 : right.look_ahead(NumberToLookAhead)
+    score_left = left.nil? ? 0 : left.look_ahead(NumberToLookAhead)
+    left_max = left.max unless left.nil? or score_right > score_left
+    right_max = right.max unless right.nil? or score_left > score_right
+    [value + (left_max || 0), value + (right_max || 0)].max
   end
-    
+  
+  def look_ahead(n)
+    return value if n == 0 or left.nil? or right.nil?
+    [value + left.look_ahead(n - 1), value + right.look_ahead(n -1)].max
+  end
 end
 
-tree = short_text.each_line {|l| l.split(" ") }
-
+values = text.map {|x| x.split(' ').map {|i| i.to_i} }
+node = Node.new(values[0][0], 0, 0, values)
+puts node.max
