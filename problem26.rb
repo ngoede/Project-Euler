7ea9c08224e1910a8e8cd72f03612f5e1d4d3622
Nick Goede <ngoede@gmail.com>
2011-05-19 16:41:25 -0400

    Added problem 26 in ruby.  Still doesn't work. Needs equals I think.

---
+++ problem26.rb
@@ -0,0 +1,47 @@
+class DivisionValue
+  attr :denominator
+  attr :digit
+  attr :remainder
+  
+  def initialize(denom, digit, remain)
+    @denominator = denom
+    @digit = digit
+    @remainder = remain
+  end
+end
+
+def division(value)
+  whole = value.remainder / value.denominator
+  accounted = whole * value.denominator
+  remain = value.remainder - accounted
+  DivisionValue.new(value.denominator, whole, remain * 10)
+end
+
+def brent(x0)
+  power = lam = 1
+  tortoise = x0
+  hare = division(x0)
+  while(tortoise.digit != hare.digit)
+    if power == lam then
+      tortoise = hare
+      power *= 2
+      lam = 0
+    end
+    hare = division(hare)
+    lam += 1
+  end
+  
+  mu = 0
+  torotise = hare = x0
+  (1..lam).each do |i|
+    hare = division(hare)
+  end
+  while tortoise.digit != hare.digit
+    tortoise = division(tortoise)
+    hare = division(hare)
+    mu += 1
+  end
+  [lam, mu]
+end
+
+p brent(DivisionValue.new 7, 0, 1)
