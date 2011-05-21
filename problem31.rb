b743330b435ac32670e8300a3be4202c2ab5cabc
Nick Goede <ngoede@gmail.com>
2011-05-21 11:49:05 -0400

    solution to 31 that is too slow

--- problem31.rb
+++ problem31.rb
@@ -1,27 +1,46 @@
-class Money
-	
-	attr :amount, :type
+require 'set'
 
-	def initialize(amount, type)
-		@amount = amount
-		@type = type
+class CoinState
+	@@already_explored = Set.new
+	CoinValues = [1, 2, 5, 10, 20, 50, 100, 200]
+
+	def initialize(coinStates)
+		@coin = coinStates
+		@@already_explored.add coinStates
 	end
 	
-	def to_s
-		if type == :pence then
-			amount += "p"
-		elsif type == :pound then
-			amount += "f"
-		else
-			raise "Invalid type"
+	def get_next
+		possible_values = (0...8).map do |x|
+			result = Array.new(@coin)
+			result[x] += 1
+			result
 		end
+		possible = possible_values.reject {|x| @@already_explored.include? x}.map {|x| CoinState.new x }
+		possible.select {|x| x.value <= 200 }
 	end
 	
-	def -(other)
-		raise "Invalid Subtraction" if self.type == :pence and self.amount < other.amount
-		raise "Invalid Subtraction" if self.type == :pence and self.amount < 100 and other.type == :pound and other.amount != 0
-		thisAmount = self.type == :pound ? self.amount * 100 : self.amount
-		otherAmount = other.type == :pound ? other.amount * 100 : other.amount
+	def value
+		@coin.zip(CoinValues).map {|x| x.reduce(:*) }.reduce(:+)
 	end
 end
 
+
+class Problem
+	attr_reader :results
+
+	def initialize
+		@results = []
+	end
+	
+	def solve(states_to_test)
+		states_to_test.each do |s|
+			next_states = s.get_next
+			@results += next_states.select {|x| x.value == 200 }
+			solve(next_states.select {|x| x.value < 200 })
+		end
+	end
+end
+
+p = Problem.new
+p.solve([CoinState.new([0, 0, 0, 0, 0, 0, 0, 0])])
+puts p.results.count
