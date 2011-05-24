b8236a1bb6b693b88a3f64779a4844224171a084
Nick Goede <ngoede@gmail.com>
2011-05-24 13:24:10 -0400

    Wow this solution is much faster and shorter.  Getting stuck sucks

--- problem31.rb
+++ problem31.rb
@@ -1,46 +1,11 @@
-require 'set'
+Coins = [1, 2, 5, 10, 20, 50, 100, 200]
 
-class CoinState
-	@@already_explored = Set.new
-	CoinValues = [1, 2, 5, 10, 20, 50, 100, 200]
-
-	def initialize(coinStates)
-		@coin = coinStates
-		@@already_explored.add coinStates
-	end
+def change(total, denoms)
+	return 1 if total == 0
+	return 0 if total < 0
+	return 0 if denoms <= 0 && total >= 1
 	
-	def get_next
-		possible_values = (0...8).map do |x|
-			result = Array.new(@coin)
-			result[x] += 1
-			result
-		end
-		possible = possible_values.reject {|x| @@already_explored.include? x}.map {|x| CoinState.new x }
-		possible.select {|x| x.value <= 200 }
-	end
-	
-	def value
-		@coin.zip(CoinValues).map {|x| x.reduce(:*) }.reduce(:+)
-	end
+	change(total, denoms - 1) + change(total - Coins[denoms - 1], denoms)
 end
 
-
-class Problem
-	attr_reader :results
-
-	def initialize
-		@results = []
-	end
-	
-	def solve(states_to_test)
-		states_to_test.each do |s|
-			next_states = s.get_next
-			@results += next_states.select {|x| x.value == 200 }
-			solve(next_states.select {|x| x.value < 200 })
-		end
-	end
-end
-
-p = Problem.new
-p.solve([CoinState.new([0, 0, 0, 0, 0, 0, 0, 0])])
-puts p.results.count
+puts change(200, Coins.count)
