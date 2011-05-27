5941f39883477386d88bb5240e6bcb3cd8d08802
Nick Goede <ngoede@gmail.com>
2011-05-27 13:24:43 -0400

    38 done. Didn't notice the result I was getting before had 0 in it.

--- problem38.rb
+++ problem38.rb
@@ -5,23 +5,22 @@ class Integer
 	end
 	
 	def pandigital?(size)
-		self.to_s.chars.to_a.uniq.length == size
+		num_digits = self.to_s.chars.to_a.uniq.map {|i| i.to_i }
+		num_digits.all? {|i| (1..size).to_a.include? i } and num_digits.count == size
 	end
 end
 
 max_pand = 0
-test_range = 1..10000000
+test_range = 1..10_000
 test_range.each do |num|
-	number_of_digits_left = 9
 	i = 1
 	pand = 0
-	until number_of_digits_left <= 0
+	until pand.number_of_digits >= 9
 		next_num = num * i
-		number_of_digits_left -= next_num.number_of_digits
 		pand = (pand * 10 ** next_num.number_of_digits) + next_num
 		i += 1
 	end
-	max_pand = pand if pand.number_of_digits == 9 and pand.pandigital?(9) and pand > max_pand
+	max_pand = pand if pand.number_of_digits == 9 and pand.pandigital?(9) and pand > max_pand and i > 1
 end
 puts max_pand
 	
