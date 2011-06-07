c1b367b422351bee9d944ee1e50ce4e1c93b8d2d
Nick Goede <ngoede@gmail.com>
2011-06-07 13:20:16 -0400

    Problem 46 done. Forgot the floor on the sqrt. *sigh*

--- problem46.rb
+++ problem46.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 def get_primes n
 	max_non_prime = Integer(Math.sqrt(n))
 	primes = Array(2..n)
@@ -12,9 +14,16 @@ end
 
 def sum_of_prime_and_square?(n, primes)
 	#Assumes increasing list of primes
-	test_primes = primes.take_while { |p| p < n }
-	test_primes.map {|t| n - t }.any? {|t| two_times_square? t }
+	 primes.take_while { |p| p < n }.map {|t| n - t }.any? { |x| x.even? and two_times_square? x }
+end
+
+def two_times_square? t
+  (Math.sqrt(t / 2).floor ** 2) * 2 == t
 end
 	
-primes = get_prime(1000)
+primes = get_primes(10_000)
 
+prime_set = SortedSet.new primes
+max = primes[-1] + 2
+test_set = SortedSet.new((3..max).step(2)) - prime_set
+p test_set.find {|n| !sum_of_prime_and_square? n, prime_set}
