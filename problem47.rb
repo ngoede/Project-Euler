477aa77f641dd3c376f5a57f08cb431a817771d4
Nick Goede <ngoede@gmail.com>
2011-06-07 21:22:53 -0400

    Problem 47 done

--- problem47.rb
+++ problem47.rb
@@ -1,15 +1,21 @@
 require "./util"
 require "set"
 
+MAX = 500_000
 def find_first_n_with_n_factors(number_of_factors)
-  factor = Util::FactoringEngine.new(10000)
-  factors_used = Set.new
-  (1..10_000).each_cons(number_of_factors).find {|nums| nums.all? {|x| n_distinct?(x, number_of_factors, factors_used, factor) } }
+  factor = Util::FactoringEngine.new(MAX)
+  
+  (1..MAX).each_cons(number_of_factors).
+    find do |nums|
+      nums.all? {|x| n_distinct?(x, number_of_factors, factor) } 
+    end
 end
 
-def n_distinct? x, number_of_factors, factors_used, factorizer
+def n_distinct? x, number_of_factors, factorizer
   factors = factorizer.factor(x)
-
+  factors.length == number_of_factors
 end
 
-puts .factor(646)
+factor = Util::FactoringEngine.new(10000)
+
+puts find_first_n_with_n_factors 4
