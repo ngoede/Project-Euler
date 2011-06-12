01102a368bdbaca8507a22086330fd7ceb1d9967
Nick Goede <ngoede@gmail.com>
2011-06-11 22:00:30 -0400

    Faster but still too slow version of 50

--- Problem50.rb
+++ Problem50.rb
@@ -1,16 +1,22 @@
 require "./util"
 require "set"
 
+def create_sum_lookup primes
+  sums = []
+  primes.each_with_index {|p, i| sums << primes[0..i].reduce(:+)}
+  return sums
+end
+
 def longest_prime_sum primes, is_prime
+  sums = create_sum_lookup primes
   result = 0
   max = 0
-  (0..primes.length).each do |x|
-    break if primes.length - x < max
-    (1...(primes.length - x)).each do |i|
-      break if primes.length - x - i + 1 < max
-      sum = primes[x..-i].reduce(:+)
-      length = primes[x..-i].length
-      if length > max and is_prime.include?(sum) then
+  prime_length = primes.length
+  (0..prime_length).each do |x|
+    (1...(prime_length - x)).each do |i|
+      length = prime_length - x - i + 1
+      sum = sums[prime_length - i] - (x == 0 ? 0 : sums[x - 1] )
+      if length > max and is_prime.include?(sum)
         result = sum
         max = length
         break
@@ -20,7 +26,7 @@ def longest_prime_sum primes, is_prime
   return result, max
 end
 
-primes = Util.get_primes 1_000_000
+primes = Util.get_primes 100_000
 is_prime = Set.new primes
 
 puts longest_prime_sum primes, is_prime
