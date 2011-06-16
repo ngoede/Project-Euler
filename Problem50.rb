b47d1b46d03de67fbca431ab83f7ea57978134d1
Nick Goede <ngoede@gmail.com>
2011-06-16 13:40:17 -0400

    Problem 50 finnally done thanks to AlexU

--- Problem50.rb
+++ Problem50.rb
@@ -3,7 +3,11 @@ require "set"
 
 def create_sum_lookup primes
   sums = []
-  primes.each_with_index {|p, i| sums << primes[0..i].reduce(:+)}
+  primes.each_with_index do |p, i|
+    sum = primes[0..i].reduce(:+)
+    break if sum > 1_000_000
+    sums << sum
+  end
   return sums
 end
 
@@ -11,7 +15,7 @@ def longest_prime_sum primes, is_prime
   sums = create_sum_lookup primes
   result = 0
   max = 0
-  prime_length = primes.length
+  prime_length = sums.length
   (0..prime_length).each do |x|
     (1...(prime_length - x)).each do |i|
       length = prime_length - x - i + 1
@@ -26,7 +30,9 @@ def longest_prime_sum primes, is_prime
   return result, max
 end
 
-primes = Util.get_primes 100_000
+now = Time.now
+primes = Util.get_primes 1_000_000
+p Time.now - now
 is_prime = Set.new primes
 
 puts longest_prime_sum primes, is_prime
