edc7f792b92e12e15d5a692ffbe5538ac7dea15d
Nick Goede <ngoede@gmail.com>
2011-06-30 14:12:02 -0400

    More work on 54

--- problem54.rb
+++ problem54.rb
@@ -36,6 +36,16 @@ class Card
   end
 end
 
+class HandType
+	def initialize(&block)
+		@block = block
+	end
+	
+	def match hand
+		block.call(hand)
+	end
+end
+
 class Hand
   attr_reader :cards
   
@@ -48,6 +58,71 @@ class Hand
     right_best_card = right.cards.sort[-1]
     self_best_card > right_best_card
   end
+end
+
+def royal_flush
+	sf = straight_flush(hand)
+	if sf.match? and sf.high_card_in_set.num == 13 then
+		sf
+	else
+		Struct.new :match? => false
+	end
+end
+
+def straight_flush
+	if flush(hand).match? and straight(hand).match? then
+		Struct.new :match? => true, :high_card_in_set => hand.cards.sort[-1], :high_card => nil
+	else
+		Struct.new :match? => false
+	end
+end
+
+def flush hand
+	if hand.cards.all? {|c| c.suit == hand.cards[0].suit } then
+		Struct.new :match? => true, :high_card_in_set => hand.cards.sort[-1], :high_card => nil
+	else
+		Struct.new :match? => false
+	end
+end
+
+def four_of_a_kind hand
+	s = sets(hand)
+	if not s.empty? and s.first.length == 4 then
+		Struct.new :match? => true, :high_card_in_set => s.first[0], :high_card => nil
+	else
+		Struct.new :match? => false
+	end
+end
+
+def three_of_a_kind hand
+	s = sets(hand)
+	if not s.empty? and s.length == 1 and s.max_by {|k,v| v.length}.length == 3 then
+		Struct.new :match? => true, :high_card_in_set =>  s.max_by {|h| h.length}[0], :high_card => nil
+	else
+		Struct.new :match? => false
+	end
+end
+
+def two_pairs hand
+	s = sets (hand)
+	if not s.empty? and s.length == 2 and s.all? { |k,v| v.length == 2 }
+		Struct.new :match? => true, :high_card_in_set =>  s.max_by {|h| h.length}[0], :high_card => hand.cards.max
+	else
+		Struct.new :match? => false
+	end
+end
+
+def sets hand
+	 hand.cards.group_by { |c| c.num }.select {|key,value| value.length > 1 }
+end
+
+def straight hand
+	sorted_hand = hand.cards.sort
+	if sorted_hand[-1].num - sorted_hand[0].num == 5 and sorted_hand.group_by {|c| c.num}.length == 5 then
+		Struct.new :match? => true, :high_card_in_set => sorted_hand[-1], :high_card => nil
+	else
+		Struct.new :match? => false
+	end
 end
 
 p1 = Hand.new %w{5D 8C 9S JS AC}
